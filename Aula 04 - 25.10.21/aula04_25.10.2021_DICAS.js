781aa57ee955913810de9dd4722eebd2a0c1528a
Rodrigo Escobar <88746440+EscobarRodrigo@users.noreply.github.com>
2021-10-26 16:56:27 -0300

    Upload pastas

---
+++ Aula 04 - 25.10.21/aula04_25.10.2021_DICAS.js
@@ -0,0 +1,30 @@
+
+//DICAS DE AULA
+// 'FUNÇÃO': "ctrl + ;"
+// selecionar as linhas  + comando "ctrl+;" significa deixar os comandos COMENTADO, ou seja vai ignorar tudo
+
+var nome_de_usuario = "Rodrigo Escobar"; // o sinal de = é uma atribuição
+console.log (nome_de_usuario);
+
+nome_de_usuario = "Olá pessoal 100%";
+console.log (nome_de_usuario);
+
+nome_de_usuario = "100";
+console.log (nome_de_usuario);
+
+
+let soma = x + y + z
+let subtracao = x-y-z
+let divisao = x/y
+let multiplicacao = x * y; // multiplicação
+console.log (soma);
+console.log (subtracao);
+console.log (divisao);
+console.log (multiplicacao);
+
+
+
+let nome = "Rodrigo Escobar"; // o sinal de "=" é uma atribuição
+let idade = 25
+console.log (nome+"-"+idade);// concatenar = icluir no console.log
+
