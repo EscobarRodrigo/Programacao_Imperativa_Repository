4abadd2a154639ba6629f844b81d58bf7287edf3
Rodrigo Escobar <88746440+EscobarRodrigo@users.noreply.github.com>
2021-10-28 20:46:58 -0300

    Atualizando

---
+++ Aula 04 - 25.10.21/aula04_ex3_ex4_exercícios.js
@@ -0,0 +1,65 @@
+
+//Exercício 03
+// parte 1
+let x = 1;
+let y = 2;
+console.log (x + y);
+
+// parte 2
+console.log (x * y);
+
+// parte 4
+let soma = x + y;
+console.log (soma);
+
+// parte 5
+ soma = "1" + 1;
+console.log (soma); // transforma o tipo número em string e concatena com a string 1, 
+
+// parte 6
+soma = "Olá" + 23;
+console.log (soma)
+
+//parte 7 (concatenar)
+let A1 = "Olá"
+let A2 = "Mundo"
+let A3 = "Cruel"
+console.log (A1 + " "+ A2 + " " +A3);
+
+
+
+//IMC - Exercício 04
+
+const nome = 'José da Silva'
+let idade = 27;
+let peso = 83.5;
+let altura = 1.70;
+let plano = "Ouro"
+let imc = peso/(altura*altura);
+
+console.log( nome+" tem "+idade+" anos e seu índice de massa corporal é de "+imc+" e seu plano é "+plano);
+
+const nome = "Carlos de Souza"
+let idade = 28;
+let peso = 80.1;
+let altura = 1.76;
+let plano = "Diamante"
+let imc = peso/(altura*altura);
+
+console.log( nome+" tem "+idade+" anos e seu índice de massa corporal é de "+imc+" e seu plano é "+plano);
+
+const nome = 'Aline Ferreira'
+let idade = 33;
+let peso = 63.7;
+let altura = 1.53;
+let plano = "Não Possui"
+let imc = peso/(altura*altura);
+
+console.log( nome+" tem "+idade+" anos e seu índice de massa corporal é de "+imc+" e seu plano é "+plano);
+
+const nome = 'Ana Paula'
+let idade = 26;
+let peso = 55.0;
+let altura = 1.62;
+let plano = "Prata"
+let imc = peso/(altura*altura);
---
+++ Aula 04 - 25.10.21/variaveis.js
@@ -0,0 +1,8 @@
+var nome_de_usuario = "Rodrigo Escobar"; // o sinal de "=" é uma atribuição
+console.log (nome_de_usuario);
+
+nome_de_usuario = "Olá pessoal 100%";
+console.log (nome_de_usuario);
+
+nome_de_usuario = "100";
+console.log (nome_de_usuario);
