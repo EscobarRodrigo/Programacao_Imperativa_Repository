3f7108e360b170b61fae5bd6389f80f2f2904eaf
Rodrigo Escobar <88746440+EscobarRodrigo@users.noreply.github.com>
2021-11-03 20:28:32 -0300

    add

---
+++ Aula 07 - 03.11.21/Aula07_Dicas.js
@@ -0,0 +1,23 @@
+
+
+console.log (31 > 30); // true
+
+//NEGAÇÃO
+console.log (!(30 >= 30) || true);
+console.log (!("" || true)); // comparação vazia quando não possui nada/vazia ""  - // false
+console.log (!(" " || true)); // Porém com espaço dentro " "= true, pois existe um carecter) // false
+
+
+// OU
+console.log ((true) || true || true); // true
+console.log ((false) || true || true); //
+console.log ((false) || false || false);
+
+
+// &&
+
+console.log ((true) && true && true);
+console.log ((false) && false && false);
+
+
+
---
+++ Aula 07 - 03.11.21/Aula07_mesadetrabalho.js
@@ -0,0 +1,40 @@
+//Exercício 01
+
+console.log (!true); // false - 01
+console.log (!false); // true - 02
+console.log (!!false); // false - 03
+console.log (!!true); // true - 04
+console.log (!1); // false - 05
+console.log (!!1); // true - 06
+console.log (!0); // true - 07
+console.log (!!0); // false - 08
+console.log (!!""); // false - 09
+
+ 
+ let X = 5;
+ let Y = 9;
+
+//A)
+console.log ((X < 10) && (X!==5)); //false
+
+//B)
+console.log ((X > 9) || (Y ===5)); //false
+
+//C)
+console.log (!(X===Y));//true
+
+// //Ex2
+
+let x = 10;
+let y = "a";
+console.log ((y==="b") || (x >= 10)); // true
+
+let a = 3; // x
+let b = 8; // y
+console.log (!(a=="3") || (a === b) && !(b!==8) && (a<=b)); //false
+
+let str = ""
+let msg =  "haha!"
+let eBonito = "false"
+
+console.log (!(str|| msg) && eBonito); //false
